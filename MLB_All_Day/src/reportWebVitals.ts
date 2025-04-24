120fe90e32ea03ff73c52720a05940b08a5fc32d
yoonhoqpt <44776698+yoonhoqpt@users.noreply.github.com>
2025-04-24 15:53:04 -0700

    n8n is adding: src/reportWebVitals.ts

---
+++ MLB_All_Day/src/reportWebVitals.ts
@@ -0,0 +1,17 @@
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .catch(error => {
+        console.error('Error loading web-vitals', error);
+      });
+  }
+};
+
+export default reportWebVitals;
